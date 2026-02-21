4e143675e62dccd8dd79437c8bab8c3cc64d6813
INONONO66 <inonono66@gmail.com>
2026-02-22 03:44:43 +0900

    chore(session): update package barrel exports for new storage modules

--- packages/session/src/storage/index.ts
+++ packages/session/src/storage/index.ts
@@ -1,2 +1,6 @@
 export { Storage, InMemoryStorage } from "./storage";
 export { FileStorageAdapter } from "./file-storage";
+export { FileLock } from "./lock";
+export { CachedStorageAdapter } from "./cache";
+export { ensureGitignore } from "./gitignore";
+export { initialize, InitializeOptions } from "./initialize";
