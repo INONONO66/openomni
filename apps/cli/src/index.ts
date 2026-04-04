7fad2cf83fac16ba7abd471f3d079b88389f9c40
INONONO66 <inonono66@gmail.com>
2026-04-05 05:00:27 +0900

    refactor(cli): remove serve command and adapter code

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -10,7 +10,6 @@ import { Storage } from "@openomni/session";
 import pkg from "../package.json";
 import { AuthCommand } from "./cmd/auth";
 import { ConfigCommand } from "./cmd/config";
-import { ServeCommand } from "./cmd/serve";
 
 ModelsDev.init();
 Storage.initialize({ cwd: homedir() });
@@ -27,7 +26,6 @@ await yargs(hideBin(process.argv))
   .alias("version", "v")
   .command(AuthCommand)
   .command(ConfigCommand)
-  .command(ServeCommand)
   .demandCommand(1, "Run a command. Try --help for usage.")
   .strict()
   .parseAsync();
