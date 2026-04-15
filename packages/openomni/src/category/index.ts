6adfddc061846d0aec6cd72ebb3bb6bb3f3c4d8a
INONONO66 <inonono66@gmail.com>
2026-04-15 10:55:50 +0900

    refactor(openomni): remove category system

--- packages/openomni/src/category/index.ts
+++
@@ -1,3 +0,0 @@
-export type { CategoryConfig, CategoryResolution } from "./category";
-export { BUILTIN_CATEGORIES } from "./builtin-categories";
-export { resolveCategory } from "./category-resolver";
