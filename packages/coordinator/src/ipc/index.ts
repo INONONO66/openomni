d71fd4a8d50c6624283c57e38a02dc3629bac3b6
INONONO66 <inonono66@gmail.com>
2026-06-14 08:53:09 +0900

    refactor(coordinator): trim ipc public surface

--- packages/coordinator/src/ipc/index.ts
+++ packages/coordinator/src/ipc/index.ts
@@ -1,5 +1,1 @@
-export { connectIpcClient, type IpcClient, type ConnectIpcClientOptions } from "./client";
-export { decodeMessage, type IpcMessage } from "./codec";
-export { IpcConnectionError, IpcProtocolError, IpcTimeoutError } from "./errors";
-export { encode, LineDecoder } from "./framing";
-export { createIpcServer, type IpcServer, type RequestHandler } from "./server";
+export { createIpcServer } from "./server";
