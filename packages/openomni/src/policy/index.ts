8930cecf4844970ead020ecaea86c3a1230d9118
INONONO <inonono66@gmail.com>
2026-06-14 06:00:39 +0900

    refactor(openomni): trim policy barrel middleware surface

--- packages/openomni/src/policy/index.ts
+++ packages/openomni/src/policy/index.ts
@@ -1,10 +1,7 @@
-export { IngressAuthorityMiddleware } from "../ingress/middleware/ingress-authority";
-export { SubagentSpawnPolicyMiddleware } from "../subagent/middleware/subagent-spawn-policy";
 /**
  * @deprecated Use `BackgroundLimitsMiddleware` from the package root for new code.
  */
 export { BackgroundLimitsPolicy } from "./background-limits";
-export { ToolRuntimePolicyMiddleware } from "../execution-runtime/tool/middleware/tool-runtime-policy";
 export { PolicyResolver } from "./resolver";
 export type {
   LabelMatcher,
