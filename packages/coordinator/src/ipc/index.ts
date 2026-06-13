7309dc013210d8ee8a6123a8bcb6112374d279db
INONONO <inonono66@gmail.com>
2026-06-14 04:32:38 +0900

    refactor(coordinator): keep ipc encoder internal

--- packages/coordinator/src/ipc/index.ts
+++ packages/coordinator/src/ipc/index.ts
@@ -1,5 +1,5 @@
 export { connectIpcClient, type IpcClient, type ConnectIpcClientOptions } from "./client";
-export { decodeMessage, encodeMessage, type IpcMessage } from "./codec";
+export { decodeMessage, type IpcMessage } from "./codec";
 export { IpcConnectionError, IpcProtocolError, IpcTimeoutError } from "./errors";
 export { encode, LineDecoder } from "./framing";
 export { createIpcServer, type IpcServer, type RequestHandler } from "./server";
