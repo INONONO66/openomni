09c2e4f793c52d1024c770cbb40204609d09ae76
INONONO66 <inonono66@gmail.com>
2026-06-14 05:23:17 +0900

    refactor(openomni): trim system tool barrel surface

--- packages/openomni/src/execution-runtime/tool/system/index.ts
+++ packages/openomni/src/execution-runtime/tool/system/index.ts
@@ -1,7 +1,1 @@
 export { SystemToolProvider } from "./provider.js";
-export { bashTool } from "../builtins/bash.js";
-export { createEditTool } from "../builtins/edit.js";
-export { createGlobTool } from "../builtins/glob.js";
-export { createGrepTool } from "../builtins/grep.js";
-export { createReadTool } from "../builtins/read.js";
-export { createWriteTool } from "../builtins/write.js";
