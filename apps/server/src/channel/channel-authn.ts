ddd6a5a3ab4e96f4f5b44495754fe790545dba65
INONONO66 <inonono66@gmail.com>
2026-06-14 10:31:29 +0900

    refactor(server): trim namespace member surface

--- apps/server/src/channel/channel-authn.ts
+++ apps/server/src/channel/channel-authn.ts
@@ -1,12 +1,5 @@
 export type { ChannelAuthnDecisionObserver } from "./authn/types";
 
-import {
-  DiscordTriggers as DiscordTriggersDefinition,
-  GitHubHmac as GitHubHmacDefinition,
-  GitHubTriggers as GitHubTriggersDefinition,
-  TelegramTriggers as TelegramTriggersDefinition,
-  WebSocketToken as WebSocketTokenDefinition,
-} from "./authn/definitions";
 import { authenticateGitHubWebhook as authenticateGitHubWebhookImpl } from "./authn/github";
 import {
   authenticateDiscordTriggers as authenticateDiscordTriggersImpl,
@@ -16,12 +9,6 @@ import {
 import { authenticateWebSocketUpgrade as authenticateWebSocketUpgradeImpl } from "./authn/websocket";
 
 export namespace ChannelAuthnMiddleware {
-  export const WebSocketToken = WebSocketTokenDefinition;
-  export const GitHubHmac = GitHubHmacDefinition;
-  export const DiscordTriggers = DiscordTriggersDefinition;
-  export const TelegramTriggers = TelegramTriggersDefinition;
-  export const GitHubTriggers = GitHubTriggersDefinition;
-
   export const authenticateDiscordTriggers = authenticateDiscordTriggersImpl;
   export const authenticateTelegramTriggers = authenticateTelegramTriggersImpl;
   export const authenticateGitHubTriggers = authenticateGitHubTriggersImpl;
