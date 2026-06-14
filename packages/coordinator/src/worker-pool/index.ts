ca6acaf5e9e52ff8e6c36bc4b1d101e22f9311d6
INONONO66 <inonono66@gmail.com>
2026-06-14 09:41:48 +0900

    refactor(coordinator): remove worker-pool barrel

--- packages/coordinator/src/worker-pool/index.ts
+++
@@ -1,8 +0,0 @@
-export { createSessionRouting, type SessionRouter } from "./session-routing";
-export {
-  WorkerSupervisor,
-  type ToolCallCancelParams,
-  type ToolCallContext,
-  type ToolCallParams,
-  type ToolCallResult,
-} from "./supervisor";
