2e3679d826d1b5942370395aa61d58c6b8c68da6
INONONO <inonono66@gmail.com>
2026-08-17 02:28:01 +0900

    refactor(ipc): the barrel carries IpcServer like IpcClient (#606)

--- packages/ipc/src/index.ts
+++ packages/ipc/src/index.ts
@@ -5,4 +5,4 @@
 export { connectIpcClient, type IpcClient } from "./client";
 export { IpcConnectionError, IpcProtocolError, IpcRemoteError, IpcTimeoutError } from "./errors";
 export { encode } from "./framing";
-export { createIpcServer } from "./server";
+export { createIpcServer, type IpcServer } from "./server";
